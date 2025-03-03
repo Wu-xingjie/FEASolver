7d1fb494ff652482dbd8e180bc166e3224d7f2cb
wuxingjie <wxj122350@126.com>
2025-03-04 00:14:40 +0800

    <fix>完善了tool和test的CMakeLists.txt,使其能够正常编译链接;通过了id_manager测试代码

--- test/tool/id_manager_test.cpp
+++ test/tool/id_manager_test.cpp
@@ -1,9 +1,26 @@
-#include "tool/id_manager/id_mananger.h"
 #include <iostream>
 
-namespace TEST {
+#include "id_manager/id_mananger.h"
+
 int main() {
-  std::cout << TOOL::IdManager(TOOL::id_type::NODE) << std::endl;
+  std::cout << ">>>>>>>NODE test" << std::endl;
+  std::cout << "NODE: " << TOOL::IdManager(TOOL::id_type::NODE) << std::endl;
+  std::cout << "NODE: " << TOOL::IdManager(TOOL::id_type::NODE) << std::endl;
+  std::cout << "NODE: " << TOOL::IdManager(TOOL::id_type::NODE) << std::endl;
+
+  std::cout << ">>>>>>>ELEMENT test" << std::endl;
+  std::cout << "ELMENT: " <<TOOL::IdManager(TOOL::id_type::ELEMENT) << std::endl;
+  std::cout << "ELMENT: " <<TOOL::IdManager(TOOL::id_type::ELEMENT) << std::endl;
+  std::cout << "ELMENT: " <<TOOL::IdManager(TOOL::id_type::ELEMENT) << std::endl;
+
+  std::cout << ">>>>>>>LOAD test" << std::endl;
+  std::cout << "LOAD: " << TOOL::IdManager(TOOL::id_type::LOAD) << std::endl;
+  std::cout << "LOAD: " << TOOL::IdManager(TOOL::id_type::LOAD) << std::endl;
+  std::cout << "LOAD: " << TOOL::IdManager(TOOL::id_type::LOAD) << std::endl;
+
+  std::cout << ">>>>>>>COORDINATE test" << std::endl;
+  std::cout << "COORDINATE: " << TOOL::IdManager(TOOL::id_type::COORDINATE) << std::endl;
+  std::cout << "COORDINATE: " << TOOL::IdManager(TOOL::id_type::COORDINATE) << std::endl;
+  std::cout << "COORDINATE: " << TOOL::IdManager(TOOL::id_type::COORDINATE) << std::endl;
   return 0;
 }
-} // namespace TEST
--- tool/id_manager/id_mananger.cpp
+++ tool/id_manager/id_mananger.cpp
@@ -1,1 +1,35 @@
 #include "id_mananger.h"
+
+static int NodeID = 0;
+static int ELEMENTID = 0;
+static int LOADID = 0;
+static int COORDINATEID = 0;
+
+int TOOL::IdManager(const TOOL::id_type &type) {
+  try {
+    int num = 0;
+    switch (type) {
+      case TOOL::id_type::NODE:
+        NodeID += 1;
+        num = NodeID;
+        break;
+      case TOOL::id_type::LOAD:
+        LOADID += 1;
+        num = LOADID;
+        break;
+      case TOOL::id_type::COORDINATE:
+        COORDINATEID += 1;
+        num = COORDINATEID;
+        break;
+      case TOOL::id_type::ELEMENT:
+        ELEMENTID += 1;
+        num = ELEMENTID;
+        break;
+      default:
+        break;
+    }
+    return num;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+}
