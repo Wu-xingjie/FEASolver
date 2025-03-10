29112bec9d2768a63d40925b1e7ab1867a608ed4
wuxingjie <wxj122350@126.com>
2025-03-10 23:47:53 +0800

    <feat>在杆单元中增加了杆单元属性

--- src/component/element/D1/ROD.cpp
+++ src/component/element/D1/ROD.cpp
@@ -1,3 +1,11 @@
 #include "ROD.h"
 
-void ELEM::Rod::NodeSet(const NODE::Node &g1, const NODE::Node &g2) {}
+namespace COMPONENT {
+void ROD::SetNode(const Node &g1, const Node &g2) {
+  _G1 = g1;
+  _G2 = g2;
+}
+
+void ROD::SetProperty(const PROD &p) { _property = p; }
+
+}  // namespace COMPONENT
--- src/component/element/D1/ROD.h
+++ src/component/element/D1/ROD.h
@@ -1,24 +1,30 @@
 #pragma once
-#include "component/element/elemen_base.h"
-#include "component/node/node.h"
 #include <eigen3/Eigen/Core>
 
-namespace ELEM {
-class Rod : public ElemBase {
-public:
-  Rod() { _loc_k.setZero(); }
-  ~Rod() = default;
+#include "element/elemen_base.h"
+#include "node/node.h"
+#include "property/PROD.h"
+
+namespace COMPONENT {
+class ROD : public ElemBase {
+ public:
+  ROD() { _loc_k.setZero(); }
+  ~ROD() = default;
 
   // 设置单元节点
-  void NodeSet(const NODE::Node &g1, const NODE::Node &g2);
+  void SetNode(const Node &g1, const Node &g2);
+  // 设置单元属性
+  void SetProperty(const PROD &p);
   // 生成单元刚度矩阵
   void GenerateK();
 
-private:
+ private:
   // 杆单元节点
-  NODE::Node _G1;
-  NODE::Node _G2;
+  Node _G1;
+  Node _G2;
+  // 杆单元属性
+  PROD _property;
   // 运用PETSC库中的矩阵来实现单元刚度矩阵
   Eigen::Matrix2d _loc_k;
 };
-} // namespace ELEM
+}  // namespace COMPONENT
