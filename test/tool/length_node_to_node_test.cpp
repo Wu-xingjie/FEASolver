cb5f8c40e8cebe0e948b4af642c36dd1b8e90eef
wxj <wxj122350@126.com>
2025-03-16 21:39:40 +0800

    <feat>增加了计算两节点间距离的函数及其测试代码

---
+++ test/tool/length_node_to_node_test.cpp
@@ -0,0 +1,32 @@
+#include "coordinate/cartesian_coordinate.h"
+#include "geometry_tool/length_node_to_node.h"
+
+int main() {
+  std::cout << "***********同一坐标系下**********" << std::endl;
+  // 创建坐标系1
+  COMPONENT::vec_3 test_origin(1, 2, 4);
+  COMPONENT::vec_3 test_vec1(1, 0, 0);
+  COMPONENT::vec_3 test_vec2(0, 0, -1);
+  COMPONENT::vec_3 test_vec3(0, 1, 0);
+  COMPONENT::CartesianCoord coord1(test_origin, test_vec1, test_vec2,
+                                   test_vec3);
+  // 创建节点
+  COMPONENT::Node n1(1, 2, 3, coord1);
+  COMPONENT::Node n2(2, 5, 6, coord1);
+
+  std::cout << "同一坐标系下的两节点距离：" << TOOL::LenNode2Node(n1, n2)
+            << std::endl;
+
+  std::cout << "***********不同坐标系下**********" << std::endl;
+  // 创建坐标系2
+  COMPONENT::vec_3 test2_origin(3, 5, 6);
+  COMPONENT::vec_3 test2_vec1(0, 0, 1);
+  COMPONENT::vec_3 test2_vec2(0, 1, 0);
+  COMPONENT::vec_3 test2_vec3(-1, 0, 0);
+  COMPONENT::CartesianCoord coord2(test2_origin, test2_vec1, test2_vec2,
+                                   test2_vec3);
+  COMPONENT::Node n3(2, 5, 6, coord2);
+  std::cout << "不同坐标系下的两节点距离：" << TOOL::LenNode2Node(n3, n2)
+            << std::endl;
+  return 0;
+}
--- tool/geometry_tool/length_node_to_node.h
+++ tool/geometry_tool/length_node_to_node.h
@@ -1,22 +1,7 @@
 #pragma once
-#include <cmath>
-
 #include "component/node/node.h"
 
 namespace TOOL {
-// 返回两个节点之间的距离
-double LenN2N(const COMPONENT::Node& n1, const COMPONENT::Node& n2) {
-  double l;
-  COMPONENT::arr3 loc_n1 = n1.get_location();
-  COMPONENT::arr3 loc_n2 = n2.get_location();
-  // 两节点在同一坐标系下的情况
-  if (n1.GetCoord() == n2.GetCoord()) {
-    double sum = 0;
-    for (int i = 0; i < 3; i++) {
-      sum += std::pow(loc_n1.at(i) - loc_n2.at(i), 2);
-    }
-    l = std::sqrt(sum);
-  } else {  // 两节点在不同坐标系下的情况。同时转换到全局坐标系，再计算距离
-  }
-}
-}  // namespace TOOL
+// 计算两节点之间的距离，可接受两节点不再同一坐标系下
+double LenNode2Node(const COMPONENT::Node &n1, const COMPONENT::Node &n2);
+} // namespace TOOL
