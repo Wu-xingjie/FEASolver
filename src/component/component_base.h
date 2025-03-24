09eefa2c6613396058c6aa712b4d2f8bf562e928
wuxingjie <wxj122350@126.com>
2025-03-24 23:55:42 +0800

    <feat>增加了元件mapper接口

--- src/component/component_base.h
+++ src/component/component_base.h
@@ -1,10 +1,15 @@
 #pragma once
-
+#include <array>
+#include <boost/any.hpp>
+#include <vector>
 namespace COMPONENT {
+typedef std::vector<std::array<boost::any, 10>> file_data;
 // 所有元件都继承自该类
 class CompBase {
  public:
   CompBase() = default;
   ~CompBase() = default;
+
+  virtual void GenerateComponent(const file_data& src_data) = 0;
 };
 }  // namespace COMPONENT
--- src/model_mapper/mapper.h
+++ src/model_mapper/mapper.h
@@ -1,12 +1,13 @@
 #pragma once
+#include "fea_model/fea_model.h"
 #include "register.h"
-
 namespace MAPPER {
-class FileParser {
-public:
-  FileParser() = default;
-  ~FileParser() = default;
-private:
+class FileToMapper {
+ public:
+  FileToMapper() = default;
+  ~FileToMapper() = default;
 
+  void mapper(MODEL::Model& model,
+              const std::vector<std::array<boost::any, 10>>& file_model);
 };
-} // namespace MAPPER
+}  // namespace MAPPER
