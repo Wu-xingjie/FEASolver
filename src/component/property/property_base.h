d901222f921ba855e89b378d3e41f5eeec2c5b6d
wuxingjie <wxj122350@126.com>
2025-04-10 23:39:54 +0800

    <fix>修改了property_base的接口

--- src/component/property/property_base.h
+++ src/component/property/property_base.h
@@ -1,15 +1,20 @@
 #pragma once
-#include "id_manager/id_mananger.h"
-#include <eigen3/Eigen/Dense>
-#include "model_mapper/register.h"
-#include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
+#include <boost/shared_ptr.hpp>
+#include <eigen3/Eigen/Dense>
+
+#include "id_manager/id_mananger.h"
+#include "model_mapper/register.h"
 
 namespace COMPONENT {
-class PropertyBase {
+class PropertyBase : public CompBase {
  public:
   PropertyBase() { _id = TOOL::IdManager(TOOL::id_type::PROPERTY); }
   ~PropertyBase() = default;
+
+  virtual void SetComp(const file_data &datas) = 0;
+  virtual int GetID() = 0;
+  virtual comp_type Type() = 0;
 
  protected:
   int _id;
