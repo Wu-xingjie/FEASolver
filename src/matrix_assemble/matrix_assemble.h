658b39d83f5ca7c06be412865813ed4c1a51d4f2
wxj <wxj122350@126.com>
2025-09-19 18:28:52 +0800

    <feat>增加了手动初始化矩阵组装器的成员方法

--- src/matrix_assemble/matrix_assemble.h
+++ src/matrix_assemble/matrix_assemble.h
@@ -8,10 +8,14 @@
 namespace ASSEMBLE {
 
 class MatrixAssemble {
- public:
+public:
+  MatrixAssemble() = default;
   MatrixAssemble(const MODEL::Model &model);
   ~MatrixAssemble() = default;
 
+  // 手动初始化矩阵组装器
+  void InitialMatrixAssemble(const MODEL::Model& model);
+  
   // 给控制方程施加约束
   void AddConstrain();
 
@@ -33,12 +37,12 @@ class MatrixAssemble {
   // 输出有效自由度序列
   std::vector<std::string> OutputValidDofSerial();
 
-  int _dof{0};  // 模型自由度
+  int _dof{0}; // 模型自由度
   // std::map<std::string, int> _dof2idx;  // 总体坐标系自由度到维度的映射关系
-  boost::bimap<std::string, int> _dof2idx;  // 总体坐标系自由度到维度的映射关系
-  MODEL::Model _model;                      // 模型库
-  Eigen::MatrixXd _matrix_k;  // 刚度矩阵
-  Eigen::VectorXd _vector_f;  // 载荷列阵
-  std::set<int> _extro_dof;   // 多余自由度
+  boost::bimap<std::string, int> _dof2idx; // 总体坐标系自由度到维度的映射关系
+  MODEL::Model _model;                     // 模型库
+  Eigen::MatrixXd _matrix_k;               // 刚度矩阵
+  Eigen::VectorXd _vector_f;               // 载荷列阵
+  std::set<int> _extro_dof;                // 多余自由度
 };
-}  // namespace ASSEMBLE
+} // namespace ASSEMBLE
