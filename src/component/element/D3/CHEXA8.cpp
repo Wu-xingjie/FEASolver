e538411a36b893c0c327fa970d1547754b7c11a1
wxj <wxj122350@126.com>
2025-09-19 15:36:11 +0800

    <fix>修复了chexa8单元部分bug

--- src/component/element/D3/CHEXA8.cpp
+++ src/component/element/D3/CHEXA8.cpp
@@ -109,21 +109,15 @@ void Chexa8::GenerateK(const MODEL::Model &model) {
     }
     auto N8_datas = N8->get_location();
 
-    auto n1_vec = Eigen::Vector2d{N1_datas(0), N1_datas(1)};
-    auto n2_vec = Eigen::Vector2d{N2_datas(0), N2_datas(1)};
-    auto n3_vec = Eigen::Vector2d{N3_datas(0), N3_datas(1)};
-    auto n4_vec = Eigen::Vector2d{N4_datas(0), N4_datas(1)};
-    auto n5_vec = Eigen::Vector2d{N5_datas(0), N5_datas(1)};
-    auto n6_vec = Eigen::Vector2d{N6_datas(0), N6_datas(1)};
-    auto n7_vec = Eigen::Vector2d{N7_datas(0), N7_datas(1)};
-    auto n8_vec = Eigen::Vector2d{N8_datas(0), N8_datas(1)};
-
-    std::array<double, 8> x_arr{n1_vec(0), n2_vec(0), n3_vec(0), n4_vec(0),
-                                n5_vec(0), n6_vec(0), n7_vec(0), n8_vec(0)};
-    std::array<double, 8> y_arr{n1_vec(1), n2_vec(1), n3_vec(1), n4_vec(1),
-                                n5_vec(1), n6_vec(1), n7_vec(1), n8_vec(1)};
-    std::array<double, 8> z_arr{n1_vec(2), n2_vec(2), n3_vec(2), n4_vec(2),
-                                n5_vec(2), n6_vec(2), n7_vec(2), n8_vec(2)};
+    std::array<double, 8> x_arr{N1_datas(0), N2_datas(0), N3_datas(0),
+                                N4_datas(0), N5_datas(0), N6_datas(0),
+                                N7_datas(0), N8_datas(0)};
+    std::array<double, 8> y_arr{N1_datas(1), N2_datas(1), N3_datas(1),
+                                N4_datas(1), N5_datas(1), N6_datas(1),
+                                N7_datas(1), N8_datas(1)};
+    std::array<double, 8> z_arr{N1_datas(2), N2_datas(2), N3_datas(2),
+                                N4_datas(2), N5_datas(2), N6_datas(2),
+                                N7_datas(2), N8_datas(2)};
     std::array<std::array<double, 8>, 3> point_arr{x_arr, y_arr, z_arr};
 
     // 等参单元在自然坐标系下单元的边长为2
@@ -226,14 +220,14 @@ void Chexa8::GenerateK(const MODEL::Model &model) {
           Eigen::Matrix3d jacob = Eigen::Matrix3d::Zero();
           for (int kk = 0; kk < 3; kk++) {
             for (int ii = 0; ii < 3; ii++) {
+              std::string str_jacob_ki = "";
               for (int jj = 0; jj < 3; jj++) {
-                std::string str_jacob_ki = "";
                 str_jacob_ki += "+(" + arr_Ni_partial.at(kk).at(jj) + ")*" +
-                                std::to_string(point_arr.at(kk).at(jj));
-                jacob(kk, ii) =
-                    TOOL::FuncCal(str_jacob_ki, gauss_sample[i],
-                                  gauss_sample.at(j), gauss_sample.at(k));
+                                std::to_string(point_arr.at(ii).at(jj));
               }
+              jacob(kk, ii) =
+                  TOOL::FuncCal(str_jacob_ki, gauss_sample[i],
+                                gauss_sample.at(j), gauss_sample.at(k));
             }
           }
 
@@ -241,7 +235,7 @@ void Chexa8::GenerateK(const MODEL::Model &model) {
           double total_weight =
               gauss_weight.at(i) * gauss_weight.at(j) * gauss_weight.at(k);
           // 获取当前积分点的B矩阵
-          Eigen::MatrixXd sub_B = Eigen::MatrixXd::Zero(24, 24);
+          Eigen::MatrixXd sub_B = Eigen::MatrixXd::Zero(6, 24);
           sub_B(0, 0) = TOOL::FuncCal(str_N1_epsilon, gauss_sample.at(i),
                                       gauss_sample.at(j), gauss_sample.at(k));
           sub_B(0, 3) = TOOL::FuncCal(str_N2_epsilon, gauss_sample.at(i),
@@ -404,85 +398,32 @@ void Chexa8::GenerateK(const MODEL::Model &model) {
 }
 
 Eigen::MatrixXd Chexa8::GetGlobalK(const MODEL::Model &model) {
-  // 获取全局坐标系
-  auto global_coord =
-      boost::make_shared<COMPONENT::GlobalCoord>()->GetGeneralCoord();
-  // 构建局部坐标系
-  // 获取节点
-  auto comp_N1 = TOOL::GetCompById(model, CompBase::comp_type::node, _G1);
-  auto N1 = boost::dynamic_pointer_cast<COMPONENT::Node>(comp_N1);
-  if (!N1) {
-    throw std::runtime_error("[ERROR]:func(Chexa8::GenerateK)>>>节点1获取失败");
+  std::map<std::string, int> dof2index;
+  std::vector<std::string> vec_dof{"x",       "y",       "z",
+                                   "theta_x", "theta_y", "theta_z"};
+
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 6; j++) {
+      std::string str_dof = std::to_string(i + 1) + "_" + vec_dof.at(j);
+      dof2index[str_dof] = i * 6 + j;
+    }
   }
-  auto N1_datas = N1->get_location();
-  auto comp_N2 = TOOL::GetCompById(model, CompBase::comp_type::node, _G2);
-  auto N2 = boost::dynamic_pointer_cast<COMPONENT::Node>(comp_N2);
-  if (!N2) {
-    throw std::runtime_error("[ERROR]:func(Chexa8::GenerateK)>>>节点2获取失败");
+  Eigen::MatrixXd k_alldof = Eigen::MatrixXd::Zero(48, 48);
+  std::map<int, std::string> k_map{
+      {0, "1_x"},  {1, "1_y"},  {2, "1_z"},  {3, "2_x"},  {4, "2_y"},
+      {5, "2_z"},  {6, "3_x"},  {7, "3_y"},  {8, "3_z"},  {9, "4_x"},
+      {10, "4_y"}, {11, "4_z"}, {12, "5_x"}, {13, "5_y"}, {14, "5_z"},
+      {15, "6_x"}, {16, "6_y"}, {17, "6_z"}, {18, "7_x"}, {19, "7_y"},
+      {20, "7_z"}, {21, "8_x"}, {22, "8_y"}, {23, "8_z"}};
+  for (int i = 0; i < 24; i++) {
+    for (int j = 0; j < 24; j++) {
+      std::string dof_row = k_map.at(i);
+      std::string dof_col = k_map.at(j);
+      int idx_col = dof2index.at(dof_col);
+      int idx_row = dof2index.at(dof_row);
+      k_alldof(idx_row, idx_col) = _loc_k(i, j);
+    }
   }
-  auto N2_datas = N2->get_location();
-  auto comp_N3 = TOOL::GetCompById(model, CompBase::comp_type::node, _G3);
-  auto N3 = boost::dynamic_pointer_cast<COMPONENT::Node>(comp_N3);
-  if (!N3) {
-    throw std::runtime_error("[ERROR]:func(Chexa8::GenerateK)>>>节点3获取失败");
-  }
-  auto N3_datas = N3->get_location();
-  auto comp_N4 = TOOL::GetCompById(model, CompBase::comp_type::node, _G4);
-  auto N4 = boost::dynamic_pointer_cast<COMPONENT::Node>(comp_N4);
-  if (!N4) {
-    throw std::runtime_error("[ERROR]:func(Chexa8::GenerateK)>>>节点4获取失败");
-  }
-  auto N4_datas = N4->get_location();
-
-  auto n1_vec = Eigen::Vector2d{N1_datas(0), N1_datas(1)};
-  auto n2_vec = Eigen::Vector2d{N2_datas(0), N2_datas(1)};
-  auto n3_vec = Eigen::Vector2d{N3_datas(0), N3_datas(1)};
-  auto n4_vec = Eigen::Vector2d{N4_datas(0), N4_datas(1)};
-  // 计算单元长和宽
-  auto length = LenOfNode(n1_vec, n2_vec);
-  auto weight = LenOfNode(n4_vec, n3_vec);
-
-  // 单元中点坐标
-  Eigen::Vector3d mid_point = Eigen::Vector3d::Zero(3);
-  mid_point(0) = (N1_datas(0) + N2_datas(0) + N3_datas(0) + N4_datas(0)) / 4;
-  mid_point(1) = (N1_datas(1) + N2_datas(1) + N3_datas(1) + N4_datas(1)) / 4;
-  mid_point(2) = (N1_datas(2) + N2_datas(2) + N3_datas(2) + N4_datas(2)) / 4;
-
-  // 计算局部坐标系坐标轴
-  auto loc_axis_x = TOOL::NodesToCoord(model, _G1, _G2)->_vec1;
-  auto loc_axis_y = TOOL::NodesToCoord(model, _G1, _G4)->_vec1;
-  auto loc_axis_z = loc_axis_x.cross(loc_axis_y);
-
-  GeneralCoord loc_coord;
-  loc_coord._coord_origin = mid_point;
-  loc_coord._dim_type = GeneralCoord::gen_coord_type::dim3;
-  loc_coord._vec1 = loc_axis_x.normalized();
-  loc_coord._vec2 = loc_axis_y.normalized();
-  loc_coord._vec3 = loc_axis_z.normalized();
-
-  // 生成坐标变换矩阵
-  auto trans_matrix_block = TOOL::TransCoordToCoord(global_coord, loc_coord);
-
-  // std::cout << "坐标变换矩阵:" << std::endl;
-  // TOOL::DisplayMatrixXd(trans_matrix_block);
-
-  Eigen::MatrixXd trans_matrix = Eigen::MatrixXd::Zero(24, 24);
-  trans_matrix.block<3, 3>(0, 0) = trans_matrix_block;
-  trans_matrix.block<3, 3>(3, 3) = trans_matrix_block;
-  trans_matrix.block<3, 3>(6, 6) = trans_matrix_block;
-  trans_matrix.block<3, 3>(9, 9) = trans_matrix_block;
-  trans_matrix.block<3, 3>(12, 12) = trans_matrix_block;
-  trans_matrix.block<3, 3>(15, 15) = trans_matrix_block;
-  trans_matrix.block<3, 3>(18, 18) = trans_matrix_block;
-  trans_matrix.block<3, 3>(21, 21) = trans_matrix_block;
-
-  // 生成全局坐标系下的单元刚度矩阵
-  // 全局坐标系下单元刚度矩阵
-  Eigen::MatrixXd global_k;
-  // TOOL::DisplayMatrixXd(trans_matrix, "trans_matrix", true);
-  global_k = trans_matrix.transpose() * _loc_k * trans_matrix;
-
-  // TOOL::DisplayMatrixXd(global_k, "global_k");
-  return global_k;
+  return k_alldof;
 }
 } // namespace COMPONENT
