275514f59e27ec80c5328887cc57f26aa33ae477
wxj <wxj122350@126.com>
2025-10-31 18:06:56 +0800

    <feat>增加了数值求解器基类,重新规划了有限元模型后的程序框架

---
+++ src/numerical_solver/numerical_solver_base.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "matrix_assemble/one_dim_varialbe_bandwidth_stroage/one_dim_storage.h"
+#include <boost/make_shared.hpp>
+
+namespace NUMERICAL {
+// 数值求解器基类
+class NumericalSolverBase {
+public:
+  NumericalSolverBase(const MODEL::Model &model) {
+    _assemble = boost::make_shared<ASSEMBLE::OneDimMatrixAssemble>(model);
+    // 在数值求解器基类基类构造函数中组装刚度矩阵，目的是让数值求解器专注于数值求解
+    _assemble->AssembleK();
+  };
+  ~NumericalSolverBase() = default;
+
+  virtual Eigen::VectorXd NumericalSolver() = 0;
+
+public:
+  boost::shared_ptr<ASSEMBLE::OneDimMatrixAssemble> _assemble;
+};
+} // namespace NUMERICAL
